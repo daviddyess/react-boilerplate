75466e28039044dcc423afbb55628a09ed3de476
daviddyess <david.dyess@gmail.com>
2020-02-05 01:28:07 -0700

    Added icons library for fontawesome

---
+++ src/icons.js
@@ -0,0 +1,29 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+// Use the imports and library.add() below to add icons
+/* 
+import {
+  faBook,
+  faChevronCircleLeft,
+  faChevronLeft,
+  faChevronCircleRight,
+  faChevronRight,
+  faChevronUp,
+  faHeart
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faHeart as farHeart
+} from '@fortawesome/free-regular-svg-icons';
+
+library.add(
+  faBook,
+  faChevronCircleLeft,
+  faChevronLeft,
+  faChevronCircleRight,
+  faChevronRight,
+  faChevronUp,
+  faHeart,
+  farHeart
+);
+*/
+export default library;
